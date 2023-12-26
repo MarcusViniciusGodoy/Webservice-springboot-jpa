86a118ca5c45f828d1e53ae2f379fd051763d9bc
Marcus Vinícius <marcusviniciusdegodoy@hotmail.com>
2023-12-26 16:10:59 -0300

    Implements User

---
+++ src/main/java/com/testecoursejava/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.testecoursejava.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.testecoursejava.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Marcus", "marcus@gmail.com", "1111111", "54321");
+		return ResponseEntity.ok().body(u);
+	}
+}
