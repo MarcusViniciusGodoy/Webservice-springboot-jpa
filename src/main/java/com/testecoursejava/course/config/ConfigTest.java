d499aca6d95338d14493fcc1fb65ca8bb1f430b0
Marcus Vinícius <marcusviniciusdegodoy@hotmail.com>
2023-12-28 17:25:01 -0300

    OrderItem Implemented

--- src/main/java/com/testecoursejava/course/config/ConfigTest.java
+++ src/main/java/com/testecoursejava/course/config/ConfigTest.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.testecoursejava.course.entities.Category;
 import com.testecoursejava.course.entities.Order;
+import com.testecoursejava.course.entities.OrderItem;
 import com.testecoursejava.course.entities.Product;
 import com.testecoursejava.course.entities.User;
 import com.testecoursejava.course.entities.enums.OrderStatus;
 import com.testecoursejava.course.repositories.CategoryRepository;
+import com.testecoursejava.course.repositories.OrderItemRepository;
 import com.testecoursejava.course.repositories.OrderRepository;
 import com.testecoursejava.course.repositories.ProductRepository;
 import com.testecoursejava.course.repositories.UserRepository;
@@ -34,14 +36,11 @@ public class ConfigTest implements CommandLineRunner {
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		User u1 = new User(null, "Marcus", "marcus@gmail.com", "988888888", "123456"); 
-		User u2 = new User(null, "Vinicius", "vinicius@gmail.com", "977777777", "654321"); 
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.CANCELED, u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
 		Category cat1 = new Category(null, "Electronics"); 
 		Category cat2 = new Category(null, "Books"); 
@@ -53,9 +52,6 @@ public class ConfigTest implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 		
-		
-		userRepository.saveAll(Arrays.asList(u1, u2));
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
@@ -65,7 +61,24 @@ public class ConfigTest implements CommandLineRunner {
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
+		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		User u1 = new User(null, "Marcus", "marcus@gmail.com", "988888888", "123456"); 
+		User u2 = new User(null, "Vinicius", "vinicius@gmail.com", "977777777", "654321"); 
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.CANCELED, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	} 
 }
